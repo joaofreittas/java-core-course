40ccfdc6bf79b2b33d032a198c1eb198000a41a2
joaofreittas <joao.frhb@gmail.com>
2024-01-09 07:26:49 -0300

    feat: add implementation threads by interface

--- src/main/java/joao/app/javacorecourse/threads/ThreadMain.java
+++ src/main/java/joao/app/javacorecourse/threads/ThreadMain.java
@@ -15,15 +15,25 @@ public class ThreadMain {
 //        t3.run();
 //        t4.run();
 
-            Thread t1 = new MyThread("A");
-            Thread t2 = new MyThread("B");
-            Thread t3 = new MyThread("C");
-            Thread t4 = new MyThread("D");
+//        Thread t1 = new MyThread("A");
+//        Thread t2 = new MyThread("B");
+//        Thread t3 = new MyThread("C");
+//        Thread t4 = new MyThread("D");
+//
+//        t1.start();
+//        t2.start();
+//        t3.start();
+//        t4.start();
 
-            t1.start();
-            t2.start();
-            t3.start();
-            t4.start();
+        Thread t1 = new Thread(new MyThreadRunnable("A"));
+        Thread t2 = new Thread(new MyThreadRunnable("B"));
+        Thread t3 = new Thread(new MyThreadRunnable("C"));
+        Thread t4 = new Thread(new MyThreadRunnable("D"));
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
 
     }
 
@@ -47,6 +57,26 @@ class MyThread extends Thread {
         }
 
 
+    }
+
+}
+
+class MyThreadRunnable implements Runnable {
+
+    private final String name;
+
+    public MyThreadRunnable(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println();
+        System.out.println(Thread.currentThread().getName());
+
+        for (int i = 0; i < 100; i++) {
+            System.out.print(name);
+        }
     }
 
 }
